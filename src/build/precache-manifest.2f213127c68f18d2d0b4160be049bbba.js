e036225c93a6b15ab864210cae1f87f9c016dc36
alexlibe95 <alexlibe95@gmail.com>
2019-09-15 23:21:00 +0300

    First Commit

---
+++ src/build/precache-manifest.2f213127c68f18d2d0b4160be049bbba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044a6722b6044ae405db8179618818fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d69ea1399e2df57713",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ce1bd4f2c24766f8807b",
+    "url": "/static/css/main.03b5aeb6.chunk.css"
+  },
+  {
+    "revision": "05d69ea1399e2df57713",
+    "url": "/static/js/2.fce064dc.chunk.js"
+  },
+  {
+    "revision": "ce1bd4f2c24766f8807b",
+    "url": "/static/js/main.71de534e.chunk.js"
+  },
+  {
+    "revision": "eff53bd3d6d20bd8d4cd",
+    "url": "/static/js/runtime~main.92bac560.js"
+  },
+  {
+    "revision": "cc7ad3d3ba4e80853304bee2dc3015da",
+    "url": "/static/media/Pepsi-Logo.cc7ad3d3.png"
+  }
+]);
